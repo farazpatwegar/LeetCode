58533b28405a01b2d00166bf19b4be5b1b3074a1
Faraz Patwegar <87069253+farazpatwegar@users.noreply.github.com>
2024-09-03 23:44:45 +0530

    Time: 2109 ms (9.02%), Space: 40.5 MB (29.73%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,17 +1,14 @@
 class Solution {
-	public boolean checkPerfectNumber(int num) {
-//		System.out.println("in method");
-		int num2 = 0;
-		for (int i = 1; i < num; i++) {
-//			System.out.println("in loop");
-			if (num % i == 0) {
-				num2 =num2+ i;
-			}
-		}
-		System.out.println(num2);
-		if (num == num2)
-			return true;
-
-		return false;
-	}
+    public boolean checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i=1;i<num;i++)
+        {
+            if(num%i==0)
+                sum+=i;
+        }
+        if(sum==num)
+            return true;
+        else
+            return false;
+    }
 }
