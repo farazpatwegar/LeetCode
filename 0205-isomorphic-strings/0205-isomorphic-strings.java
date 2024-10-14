c634b532d3bb88a05a2b50e86faba0c6a786fede
Faraz Patwegar <87069253+farazpatwegar@users.noreply.github.com>
2024-10-14 21:49:08 +0530

    Time: 17 ms (30.31%), Space: 42.5 MB (26.59%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+            if (s.length() != t.length()) {
+                        return false;
+                                }
+                                        
+                                                HashMap<Character, Character> sToT = new HashMap<>();
+                                                        HashMap<Character, Character> tToS = new HashMap<>();
+                                                                
+                                                                        for (int i = 0; i < s.length(); i++) {
+                                                                                    char sChar = s.charAt(i);
+                                                                                                char tChar = t.charAt(i);
+                                                                                                            
+                                                                                                                        // Check if the mapping from s to t is consistent
+                                                                                                                                    if (sToT.containsKey(sChar)) {
+                                                                                                                                                    if (sToT.get(sChar) != tChar) {
+                                                                                                                                                                        return false;
+                                                                                                                                                                                        }
+                                                                                                                                                                                                    } else {
+                                                                                                                                                                                                                    sToT.put(sChar, tChar);
+                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                            
+                                                                                                                                                                                                                                                        // Check if the mapping from t to s is consistent
+                                                                                                                                                                                                                                                                    if (tToS.containsKey(tChar)) {
+                                                                                                                                                                                                                                                                                    if (tToS.get(tChar) != sChar) {
+                                                                                                                                                                                                                                                                                                        return false;
+                                                                                                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                                                                                                                    } else {
+                                                                                                                                                                                                                                                                                                                                                    tToS.put(tChar, sChar);
+                                                                                                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                                                                                                                                                                
+                                                                                                                                                                                                                                                                                                                                                                                        return true;
+                                                                                                                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                                                                                                                            
